2e14a13806dd487317c6088b2652d683ae917d1e
Haris Sucipto <harisssucipto@gmail.com>
2019-04-24 22:24:28 +0700

    main init

---
+++ App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import { AppLoading, Font } from 'expo';
+
+export default class App extends React.Component {
+  state = {
+    theme: null,
+    currentTheme: null,
+    isReady: false
+  };
+  changeTheme = (theme, currentTheme) => {
+    this.setState({ theme, currentTheme });
+  };
+  async componentDidMount() {
+    await Font.loadAsync(
+      'antoutline',
+      // eslint-disable-next-line
+      require('@ant-design/icons-react-native/fonts/antoutline.ttf')
+    );
+
+    await Font.loadAsync(
+      'antfill',
+      // eslint-disable-next-line
+      require('@ant-design/icons-react-native/fonts/antfill.ttf')
+    );
+
+    // eslint-disable-next-line
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    const { theme, currentTheme, isReady } = this.state;
+    if (!isReady) {
+      return <AppLoading />;
+    }
+
+    return <HomeScreen />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+import { IconFill, IconOutline } from '@ant-design/icons-react-native';
+
+const Background = styled.View`
+  background-color: 'rgba(20, 10, 38, 1)';
+  flex: 1;
+`;
+
+const Header = styled.View`
+  height: 140px;
+  padding-top: 55px;
+  padding-left: 17px;
+  flex-direction: row;
+  background-color: rgba(34, 23, 56, 1);
+`;
+
+const H1 = styled.Text`
+  color: #fff;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 23px;
+`;
+
+const H2 = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  font-weight: 100;
+  text-align: center;
+  line-height: 23px;
+`;
+
+const H3 = styled.Text`
+  color: #fff;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 23px;
+`;
+
+const Content = styled.ScrollView`
+  flex: 1;
+  height: 100%;
+`;
+
+const Body = styled.View`
+  flex: 1;
+  position: relative;
+`;
+
+const BottomContainer = styled.View`
+  position: absolute;
+  bottom: 25;
+  right: 10;
+`;
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <Background>
+        <Header>
+          <TouchableOpacity>
+            <IconFill name="home" size={20} color="white" />
+          </TouchableOpacity>
+
+          <View style={{ flex: 1 }}>
+            <H1>AFIKA</H1>
+            <H2>Aplikasi Penghapal Al-Quran</H2>
+            <H3>0 / 114 Surah ditambahkan</H3>
+          </View>
+        </Header>
+
+        <Body>
+          <Content>
+            <H3>Test</H3>
+          </Content>
+          <BottomContainer>
+            <TouchableOpacity>
+              <IconFill name="plus-circle" size={58} color="red" />
+            </TouchableOpacity>
+          </BottomContainer>
+        </Body>
+      </Background>
+    );
+  }
+}
